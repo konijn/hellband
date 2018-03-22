fad4959b4c2651152d22c9919c05fdc33e8722b3
konijn <konijn@gmail.com>
2018-03-22 17:57:22 +0000

    Start of using the full screen

--- src/defines.h
+++ src/defines.h
@@ -793,80 +793,8 @@ and tables.c --TY */
 #define ROW_INFO                22
 #define COL_INFO                0       /* "Monster Health Bar" */
 
-
-#define ROW_TIME                1
-#define COL_TIME               0       /* 6:00AM */
-
-#define ROW_DATE               2
-#define COL_DATE               0       /* <template name> */
-
-#define ROW_TITLE               3
-#define COL_TITLE               0       /* <title> or <mode> */
-
-#define ROW_LEVEL               4
-#define COL_LEVEL               0       /* "LEVEL xxxxxx" */
-
-#define ROW_EXP                 5
-#define COL_EXP                 0       /* "EXP xxxxxxxx" */
-
-#define ROW_GOLD                6
-#define COL_GOLD                0       /* "AU xxxxxxxxx" */
-
 #define ROW_EQUIPPY             7
 #define COL_EQUIPPY             0       /* equippy chars */
-
-#define ROW_STAT                8
-#define COL_STAT                0       /* "xxx   xxxxxx" */
-
-#define ROW_AC                  15
-#define COL_AC                  0       /* "Cur AC xxxxx" */
-
-#define ROW_MAXHP               16
-#define COL_MAXHP               0       /* "Max HP xxxxx" */
-
-#define ROW_CURHP               17
-#define COL_CURHP               0       /* "Cur HP xxxxx" */
-
-#define ROW_MAXSP               18
-#define COL_MAXSP               0       /* "Max SP xxxxx" */
-
-#define ROW_CURSP               19
-#define COL_CURSP               0       /* "Cur SP xxxxx" */
-
-#define ROW_CUT                 21
-#define COL_CUT                 0       /* <cut> */
-
-#define ROW_STUN                22
-#define COL_STUN                0       /* <stun> */
-
-#define ROW_HUNGRY              23
-#define COL_HUNGRY              0       /* "Weak" / "Hungry" / "Full" / "Gorged" */
-
-#define ROW_BLIND               23
-#define COL_BLIND               7       /* "Blind" */
-
-#define ROW_CONFUSED    23
-#define COL_CONFUSED    13      /* "Confused" */
-
-#define ROW_AFRAID              23
-#define COL_AFRAID              22      /* "Afraid" */
-
-#define ROW_POISONED    23
-#define COL_POISONED    29      /* "Poisoned" */
-
-#define ROW_STATE               23
-#define COL_STATE               38      /* <state> */
-
-#define ROW_SPEED               23
-#define COL_SPEED               49      /* "Slow (-NN)" or "Fast (+NN)" */
-
-#define ROW_STUDY               23
-#define COL_STUDY               58      /* "Study" */
-
-#define ROW_DEPTH               23
-#define COL_DEPTH               67      /* "Lev NNN" / "NNNN ft" */
-
-
 
 /*** Terrain Feature Indexes (see "lib/edit/f_info.txt") ***/
 
